e2e4844c7ce1637a895369f86a80d22a7ed08305
Allan ROLLE <allan.rolle@laposte.net>
2019-04-02 15:40:42 +0200

    Add script to remove focus (section projects)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
+
+  // Charts
+
     $('.chart').easyPieChart({
       barColor: "#FFFFFF",
       trackColor: "#7b6c63",
       size: 180
     });
+
+  
+  // Projects
+
+  $('.projects__thumbnails-item').on('mouseleave', function(){
+    var $this = $(this);
+    $this.blur();
+  });
+
 });
