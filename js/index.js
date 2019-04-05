dc62f99b2a6fe0895f8a4cd0148fdad7b9e6c913
Allan ROLLE <allan.rolle@laposte.net>
2019-04-05 20:54:26 +0200

    Add slider script (section testimonials)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
 
-  // Charts
+    /*
+    * Description:
+      * All the scripts
+    * Sections:
+      * $. Charts
+      * $. Projects
+      * $. Slider Testimonials
+  */
+
+
+
+
+  /* $. Charts
+  \*----------------------------------------------------------------*/
 
     $('.chart').easyPieChart({
       barColor: "#FFFFFF",
@@ -8,12 +21,52 @@ $(document).ready(function(){
       size: 180
     });
 
-  
-  // Projects
+
+    
+
+
+  /* $. Projects
+  \*----------------------------------------------------------------*/
 
   $('.projects__thumbnails-item').on('mouseleave', function(){
     var $this = $(this);
     $this.blur();
+  });
+
+
+
+
+
+ /* $. Slider Testimonials
+  \*----------------------------------------------------------------*/
+
+  var $wrapper  =  $('.testimonials__wrapper');
+  var $content  =  $('.testimonials__content');
+  var $inner    =  $('.testimonials__inner');
+  var $controls =  $('.testimonials__button');
+
+  var $innerHeight =  $inner.outerHeight(true);
+  var $width       =  $content.children().width() * $content.children().length * $content.children().length;
+
+  $wrapper.height($innerHeight + 'px');
+  $content.width($width);
+  $inner.width($width / 3);
+
+
+  $controls.on('click', function(){
+
+    var $this   =  $(this);
+    var $choice =  Number($(this).attr('data-number'));
+    var $length =  Math.round($width);
+    var $val    =  $length / 3 * $choice + 'px';
+
+    $controls.not($this).removeClass('active');
+    $this.addClass('active');
+
+    $content.css({
+      "transform": "translateX(-" + $val +")"
+    });
+
   });
 
 });
